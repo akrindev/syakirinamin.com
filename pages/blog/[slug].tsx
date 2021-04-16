1fda898e7522f4f3d9c52eedefb20c19a014c546
Syakirin Amin <33445459+akrindev@users.noreply.github.com>
2021-04-16 23:37:48 +0700

    revalidate

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,88 +1,86 @@
 import { getPosts, Posts } from "@/lib/notion";
 import { GetStaticPaths, GetStaticProps } from "next";
 import { NotionAPI } from "notion-client";
-import { getPageProperty, getDateValue } from "notion-utils";
 import Layout from "@/components/Layout";
 import ArrowLeft from "@/components/ArrowLeft";
 import NotionPage from "@/components/NotionPage";
 import Head from "next/head";
 
 const badges = {
-    laravel: "bg-rose-200 text-rose-500",
-    "react.js": "bg-blue-100 text-blue-500",
-    "next.js": "bg-gray-100 text-gray-700",
-    "vue.js": "bg-green-200 text-green-500",
-    javascript: "bg-amber-200 text-amber-800",
-    typescript: "bg-blue-400 text-gray-200"
+  laravel: "bg-rose-200 text-rose-500",
+  "react.js": "bg-blue-100 text-blue-500",
+  "next.js": "bg-gray-100 text-gray-700",
+  "vue.js": "bg-green-200 text-green-500",
+  javascript: "bg-amber-200 text-amber-800",
+  typescript: "bg-blue-400 text-gray-200"
 };
 
 const getBadge = (badge: string) => {
-    return badges[badge] ?? "bg-amber-200 text-amber-800";
+  return badges[badge] ?? "bg-amber-200 text-amber-800";
 };
 
 export default function BlogPost({ post, property }) {
-    return (
-        <Layout>
-            <Head>
-                <title>{property.title}</title>
-            </Head>
+  return (
+    <Layout>
+      <Head>
+        <title>{property.title}</title>
+      </Head>
 
-            <ArrowLeft />
-            <div className="flex flex-col justify-center text-center pb-5">
-                <h1 className="mb-3 font-bold text-2xl">{property.title}</h1>
-                <div className="flex items-center justify-center space-x-2">
-                    {property.tags &&
-                        property.tags.map((tag: string) => (
-                            <span
-                                key={tag}
-                                className={`px-4 py-0.5 rounded-3xl text-sm font-light ${getBadge(
-                                    tag.toLocaleLowerCase()
-                                )}`}
-                            >
-                                {tag.toLocaleLowerCase()}
-                            </span>
-                        ))}
-                </div>
-                <div className="font-light text-sm italic flex justify-center py-2">
-                    {property.date}
-                </div>
-            </div>
-            <div className="relative px-2 mb-20">
-                <NotionPage recordMap={post} />
-            </div>
-        </Layout>
-    );
+      <ArrowLeft />
+      <div className="flex flex-col justify-center text-center pb-5">
+        <h1 className="mb-3 font-bold text-2xl">{property.title}</h1>
+        <div className="flex items-center justify-center space-x-2">
+          {property.tags &&
+            property.tags.map((tag: string) => (
+              <span
+                key={tag}
+                className={`px-4 py-0.5 rounded-3xl text-sm font-light ${getBadge(
+                  tag.toLocaleLowerCase()
+                )}`}
+              >
+                {tag.toLocaleLowerCase()}
+              </span>
+            ))}
+        </div>
+        <div className="font-light text-sm italic flex justify-center py-2">
+          {property.date}
+        </div>
+      </div>
+      <div className="relative px-2 mb-20">
+        <NotionPage recordMap={post} />
+      </div>
+    </Layout>
+  );
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
-    const posts = await getPosts<Posts[]>();
-    const paths = posts
-        .filter((p) => p.published)
-        .map((p) => `/blog/${p.slug}`);
+  const posts = await getPosts<Posts[]>();
+  const paths = posts.filter((p) => p.published).map((p) => `/blog/${p.slug}`);
 
-    return {
-        paths,
-        fallback: true
-    };
+  return {
+    paths,
+    fallback: true
+  };
 };
 
 export const getStaticProps: GetStaticProps = async (ctx) => {
-    const posts = await getPosts<Posts[]>();
+  const posts = await getPosts<Posts[]>();
 
-    const slug = ctx.params.slug as string;
-    const notion = new NotionAPI();
-    const postPublished = posts
-        .filter((p) => p.published)
-        .find((p) => p.slug == slug);
-    const post = await notion.getPage(postPublished.id);
+  const slug = ctx.params.slug as string;
+  const notion = new NotionAPI();
+  const postPublished = posts
+    .filter((p) => p.published)
+    .find((p) => p.slug === slug);
+  const post = await notion.getPage(postPublished.id);
 
-    // const keys = Object.keys(post?.block || {});
-    // const block = post?.block?.[keys[0]]?.value;
+  // const keys = Object.keys(post?.block || {});
+  // const block = post?.block?.[keys[0]]?.value;
 
-    return {
-        props: {
-            post,
-            property: postPublished
-        }
-    };
+  return {
+    props: {
+      post,
+      property: postPublished
+    },
+    revalidate: 3
+  };
 };
--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -5,35 +5,33 @@ import Head from "next/head";
 import { getPosts, Posts } from "../../lib/notion";
 
 export default function index({ posts }) {
-    return (
-        <Layout>
-            <Head>
-                <title>Blog</title>
-            </Head>
-            <div className="flex flex-col my-5 p-5">
-                <h1 className="font-extrabold text-2xl">Blog</h1>
-                <div className="text-gray-500 leading-tight">
-                    Sharing is caring
-                </div>
-            </div>
-            <div className="mb-20 divide-y-2 divide-gray-100">
-                {posts &&
-                    posts.map((post) => <PostList key={post.id} post={post} />)}
-            </div>
-            <div className="flex justify-center py-10 text-gray-300">
-                {new Date().getFullYear()}
-            </div>
-        </Layout>
-    );
+  return (
+    <Layout>
+      <Head>
+        <title>Blog</title>
+      </Head>
+      <div className="flex flex-col my-5 p-5">
+        <h1 className="font-extrabold text-2xl">Blog</h1>
+        <div className="text-gray-500 leading-tight">Sharing is caring</div>
+      </div>
+      <div className="mb-20 divide-y-2 divide-gray-100">
+        {posts && posts.map((post) => <PostList key={post.id} post={post} />)}
+      </div>
+      <div className="flex justify-center py-10 text-gray-300">
+        {new Date().getFullYear()}
+      </div>
+    </Layout>
+  );
 }
 
 export const getStaticProps: GetStaticProps = async (ctx) => {
-    const getPublishedPosts: Posts[] = await getPosts();
-    const posts = getPublishedPosts.filter((p) => p.published);
+  const getPublishedPosts: Posts[] = await getPosts();
+  const posts = getPublishedPosts.filter((p) => p.published);
 
-    return {
-        props: {
-            posts
-        }
-    };
+  return {
+    props: {
+      posts
+    },
+    revalidate: 1
+  };
 };
