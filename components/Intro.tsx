021304f580c77c139e5ab1748c04339dac461f8d
Syakirin Amin <33445459+akrindev@users.noreply.github.com>
2021-09-05 13:39:50 +0700

    initial commit

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -3,21 +3,36 @@ import Image from "next/image";
 export default function Intro() {
   return (
     <>
-      <div className='h-full p-3 mt-5 md:mt-8 md:px-0 md:flex md:items-center md:justify-center mx-auto'>
-        <div className='w-full flex item-center justify-center'>
+      <div className="p-3 flex flex-col md:flex-row-reverse items-center justify-center mt-5">
+        <div className="w-full flex items-center justify-center">
           <Image
-            src='http://placekitten.com/300/300'
-            alt='kitten'
+            src="/avatar.svg"
+            alt="avatar"
             width={150}
             height={150}
-            className='rounded-full'
+            className="shadow rounded-full"
           />
         </div>
-        <div className='py-5'>
-          <strong>Extroverted Introvert</strong>, So many things I would do
-          every moments like learning, disscussion, coding, solving things with
-          code, and more. Currently learning Laravel, Reactjs, React Native,
-          Nextjs, Tailwindcss but not limited to.
+        <div className="py-5">
+          <div className="text-2xl font-bold block leading-normal">
+            Hi, I'm Syakirin Amin
+          </div>
+          <div className="font-inter">
+            A student and a software engineer focused on web technology. I
+            mostly use
+            <code className="font-mono text-red-500 rounded p-0.5">
+              `laravel`
+            </code>
+            ,
+            <code className="font-mono text-sky-500 rounded p-0.5">
+              `reactjs`
+            </code>
+            , and still exploring good stuff web technology. currently admiring
+            <code className="font-mono text-green-500 rounded p-0.5">
+              `vuejs`
+            </code>
+            and its community.
+          </div>
         </div>
       </div>
     </>
