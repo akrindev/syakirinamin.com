cedea31ab9ca43401183e69408befbc6d65e6f16
akrindev <akrinmin@gmail.com>
2023-04-18 08:49:22 +0700

    upgrade to nextjs 13

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -24,22 +24,22 @@ export default function Navbar() {
       <div className='w-full bg-gradient-to-r from-rose-500 via-purple-600 to-fuchsia-700 h-1'></div>
       <nav className='w-full max-w-4xl mx-auto flex flex-col'>
         <div className='p-3 bg-gradient-to-r from-rose-600 to-pink-400 text-transparent bg-clip-text text-xl'>
-          <Link href='/'>
-            <a className='hover:text-rose-800'>Hello.</a>
+          <Link href='/' className='hover:text-rose-800'>
+            Hello.
           </Link>
         </div>
         <div className='p-2 space-x-1 flex justify-end'>
           {listMenu.map((list) => {
             return (
-              <Link href={list.link} key={list.name}>
-                <a
-                  className={
-                    router.pathname == list.link
-                      ? `py-1 px-4 border-b-2 border-rose-500 text-rose-500 font-normal text-md`
-                      : `py-1 px-4 text-gray-400 font-light text-sm border-b-2 border-warmGray-100 hover:border-rose-400 hover:text-rose-400`
-                  }>
-                  {list.name}
-                </a>
+              <Link
+                className={
+                  router.pathname == list.link
+                    ? `py-1 px-4 border-b-2 border-rose-500 text-rose-500 font-normal text-md`
+                    : `py-1 px-4 text-gray-400 font-light text-sm border-b-2 border-warmGray-100 hover:border-rose-400 hover:text-rose-400`
+                }
+                href={list.link}
+                key={list.name}>
+                {list.name}
               </Link>
             );
           })}
